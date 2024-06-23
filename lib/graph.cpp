08a4e6bbabb169a85dd5bee7f7dec7cbcee74779
Akash Yadav <akash.yadav@tum.de>
2024-06-23 12:21:25 +0200

    Added subgraph functionality

--- lib/graph.cpp
+++ lib/graph.cpp
@@ -22,18 +22,34 @@ Graph::~Graph() {
 }
 
 void Graph::draw(const string &filename) {
+    // Create a new graph
     Agraph_t *g = agopen(const_cast<char*>("g"), Agundirected, NULL);
+
+    // Create a map to store subgraphs for each label
+    map<int, Agraph_t*> subgraph_map;
+
+    // Create a map to store the Agnode_t* for each node id
     map<int, Agnode_t*> agnode_map;
 
-    // Add nodes in Graphviz
+    // Create subgraphs for each label
     for (const auto &node: nodes) {
-        Agnode_t *agnod = agnode(g, const_cast<char*>(to_string(node.id).c_str()), 1);
+        if(subgraph_map.find(node.label) == subgraph_map.end()) {
+            string subgraph_name = "cluster_" + to_string(node.label);
+            Agraph_t *subgraph = agsubg(g, const_cast<char*>(subgraph_name.c_str()), 1);
+            subgraph_map[node.label] = subgraph;
+        }
+    }
+
+    // Add nodes to the subgraphs in Graphviz
+    for (const auto &node: nodes) {
+        string  node_name = to_string(node.id);
+        Agraph_t *subgraph = subgraph_map[node.label];
+        Agnode_t *agnod = agnode(subgraph, const_cast<char*>(to_string(node.id).c_str()), 1);
         agnode_map[node.id] = agnod;
 
         string node_color = getNodeColor(node.label);
 
-        // Set node color (for example, red)
-        // TODO: Each community has a different color
+        // Set node color
         agsafeset(agnod, const_cast<char*>("color"), const_cast<char*>(node_color.c_str()), const_cast<char*>(""));
         agsafeset(agnod, const_cast<char*>("style"), const_cast<char*>("filled"), const_cast<char*>(""));
         agsafeset(agnod, const_cast<char*>("fillcolor"), const_cast<char*>(node_color.c_str()), const_cast<char*>(""));
