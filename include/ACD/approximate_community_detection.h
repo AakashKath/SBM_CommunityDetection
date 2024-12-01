9526963f2b9964acb67dec267b73c7262f045481
Akash Yadav <akash.yadav@tum.de>
2024-12-01 11:29:55 +0100

    acd working as expected, need to optimise few storages and accesses

--- include/ACD/approximate_community_detection.h
+++ include/ACD/approximate_community_detection.h
@@ -6,31 +6,13 @@
 #include <random>
 #include <unordered_set>
 #include <numeric>
+#include "utils/utilities.h"
 
 using namespace std;
 
 
-class Community {
-    private:
-        struct EdgeComparator {
-            bool operator()(const pair<int, double>& left, const pair<int, double>& right) {
-                return left.second < right.second;  // Max-heap: higher weight comes first
-            }
-        };
-
-    public:
-        int e_in = 0;
-        int e_out = 0;
-        vector<Node*> nodes;
-        // Nodes within the community with edges going out of the community
-        priority_queue<pair<int, double>, vector<pair<int, double>>, EdgeComparator> nodes_to_be_removed;
-        // Nodes out of the community with edges coming into the community
-        priority_queue<pair<int, double>, vector<pair<int, double>>, EdgeComparator> nodes_to_be_added;
-};
-
 class ApproximateCommunityDetection {
     private:
-        Graph acd_graph;
         unordered_map<int, Community> communities;
         int communityCount;
         int totalEdges;
@@ -40,10 +22,10 @@ class ApproximateCommunityDetection {
         void createCommunities();
         void swapNodes(int community_label);
         bool nodeSwapAllowed(int community_label);
-        double modularityContributionByCommunity(const Community& comm);
-        double getModularity(int e_in, int e_out, int total_edges);
 
     public:
+        Graph acd_graph;
+
         ApproximateCommunityDetection(Graph graph, int communityCount, vector<pair<int, int>> addedEdges, vector<pair<int, int>> removedEdges);
         ~ApproximateCommunityDetection();
 };
--- src/approximate_community_detection.cpp
+++ src/approximate_community_detection.cpp
@@ -19,9 +19,9 @@ ApproximateCommunityDetection::ApproximateCommunityDetection(
 
     // TODO: Need to replace with change in modularity
     double old_mod = 0.0;
-    double mod = newmansModularity(acd_graph, totalEdges);
+    double mod = newmansModularity(acd_graph);
     do {
-        vector<int> randomised_communities(communityCount - 1);
+        vector<int> randomised_communities(communityCount);
         iota(randomised_communities.begin(), randomised_communities.end(), 0); // Fill with 0, 1, ..., communityCount - 1
         shuffle(randomised_communities.begin(), randomised_communities.end(), gen);
         for (const auto& community_label: randomised_communities) {
@@ -30,7 +30,7 @@ ApproximateCommunityDetection::ApproximateCommunityDetection(
             }
         }
         old_mod = mod;
-        mod = newmansModularity(acd_graph, totalEdges);
+        mod = newmansModularity(acd_graph);
     } while (mod > old_mod);
 }
 
@@ -69,9 +69,9 @@ void ApproximateCommunityDetection::createCommunities() {
         Community& comm = [&]() -> Community& {
             try {
                 return communities.at(node->label);
-            } catch (const std::out_of_range&) {
+            } catch (const out_of_range&) {
                 Community new_comm;
-                return communities.emplace(node->label, std::move(new_comm)).first->second;
+                return communities.emplace(node->label, move(new_comm)).first->second;
             }
         }();
 
@@ -89,7 +89,7 @@ void ApproximateCommunityDetection::createCommunities() {
         comm.second.e_in /= 2;
     }
 
-    // Iterate through communities to fill out/in-edge priority queues
+    // Iterate through communities to fill out/in-edge heapAndMap
     for (auto& [community_label, comm]: communities) {
         unordered_map<int, int> incoming_edge_weight;
         unordered_map<int, int> outgoing_edge_weight;
@@ -116,14 +116,18 @@ void ApproximateCommunityDetection::createCommunities() {
         }
 
         // Fill nodes_to_be_removed
-        for (const auto& [neighbor_id, outgoing_weight]: outgoing_edge_weight) {
-            comm.nodes_to_be_removed.emplace(neighbor_id, static_cast<double>(outgoing_weight) / degrees.at(neighbor_id));
+        vector<pair<int, double>> outgoing_weight_vec;
+        for (auto& [neighbor_id, outgoing_weight]: outgoing_edge_weight) {
+            outgoing_weight_vec.emplace_back(neighbor_id, static_cast<double>(outgoing_weight) / degrees.at(neighbor_id));
         }
+        comm.nodes_to_be_removed.populateHeapAndMap(outgoing_weight_vec);
 
         // Fill nodes_to_be_added
-        for (const auto& [neighbor_id, incoming_weight]: incoming_edge_weight) {
-            comm.nodes_to_be_added.emplace(neighbor_id, static_cast<double>(incoming_weight) / degrees.at(neighbor_id));
+        vector<pair<int, double>> incoming_weight_vec;
+        for (auto& [neighbor_id, incoming_weight]: incoming_edge_weight) {
+            incoming_weight_vec.emplace_back(neighbor_id, static_cast<double>(incoming_weight) / degrees.at(neighbor_id));
         }
+        comm.nodes_to_be_added.populateHeapAndMap(incoming_weight_vec);
     }
 }
 
@@ -131,44 +135,21 @@ void ApproximateCommunityDetection::swapNodes(int community_label) {
     try {
         Community& comm = communities.at(community_label);
 
-        // Get top elements from priority queues
-        Node* node_removed = acd_graph.getNode(comm.nodes_to_be_removed.top().first);
-        Node* node_added = acd_graph.getNode(comm.nodes_to_be_added.top().first);
+        // Get top elements from heapAndMap
+        Node* node_removed = acd_graph.getNode(comm.nodes_to_be_removed.getMaxElementId());
+        Node* node_added = acd_graph.getNode(comm.nodes_to_be_added.getMaxElementId());
 
         // Get the community of the added node
         int involved_comm_label = node_added->label;
         Community& involved_comm = communities.at(involved_comm_label);
 
         // Remove nodes from current community queues
-        comm.nodes_to_be_removed.pop();
-        comm.nodes_to_be_added.pop();
+        comm.nodes_to_be_removed.popElement();
+        comm.nodes_to_be_added.popElement();
 
         // Remove nodes from involved community queues
-        vector<pair<int, double>> temp_pq_storage{};
-        while(!involved_comm.nodes_to_be_removed.empty()) {
-            auto queue_element = involved_comm.nodes_to_be_removed.top();
-            involved_comm.nodes_to_be_removed.pop();
-            if (queue_element.first == node_added->id) {
-                break;
-            }
-            temp_pq_storage.emplace_back(queue_element);
-        }
-        for (const auto& element: temp_pq_storage) {
-            involved_comm.nodes_to_be_removed.emplace(element);
-        }
-
-        temp_pq_storage = vector<pair<int, double>> {};
-        while(!involved_comm.nodes_to_be_added.empty()) {
-            auto queue_element = involved_comm.nodes_to_be_added.top();
-            involved_comm.nodes_to_be_added.pop();
-            if (queue_element.first == node_removed->id) {
-                break;
-            }
-            temp_pq_storage.emplace_back(queue_element);
-        }
-        for (const auto& element: temp_pq_storage) {
-            involved_comm.nodes_to_be_added.emplace(element);
-        }
+        involved_comm.nodes_to_be_removed.deleteElement(node_added->id);
+        involved_comm.nodes_to_be_added.deleteElement(node_removed->id);
 
         // Calculate updated weights for node_removed
         int old_community_edges = 0;
@@ -186,10 +167,10 @@ void ApproximateCommunityDetection::swapNodes(int community_label) {
 
         // Push the node into respective queues
         if (new_community_edges > 0) {
-            involved_comm.nodes_to_be_removed.emplace(node_removed->id, static_cast<double>(new_community_edges) / degree);
+            involved_comm.nodes_to_be_removed.insertElement(node_removed->id, static_cast<double>(new_community_edges) / degree);
         }
         if (old_community_edges > 0) {
-            comm.nodes_to_be_added.emplace(node_removed->id, static_cast<double>(old_community_edges) / degree);
+            comm.nodes_to_be_added.insertElement(node_removed->id, static_cast<double>(old_community_edges) / degree);
         }
 
         // Calculate updated weights for node_added
@@ -208,10 +189,10 @@ void ApproximateCommunityDetection::swapNodes(int community_label) {
 
         // Push the node into respective queues
         if (old_community_edges > 0) {
-            involved_comm.nodes_to_be_added.emplace(node_added->id, static_cast<double>(old_community_edges) / degree);
+            involved_comm.nodes_to_be_added.insertElement(node_added->id, static_cast<double>(old_community_edges) / degree);
         }
         if (new_community_edges > 0) {
-            comm.nodes_to_be_removed.emplace(node_added->id, static_cast<double>(new_community_edges) / degree);
+            comm.nodes_to_be_removed.insertElement(node_added->id, static_cast<double>(new_community_edges) / degree);
         }
 
         // Update node communities
@@ -232,26 +213,33 @@ bool ApproximateCommunityDetection::nodeSwapAllowed(int community_label) {
     try{
         Community& comm = communities.at(community_label);
 
-        // Get top elements from priority queues
-        Node* node_removed = acd_graph.getNode(comm.nodes_to_be_removed.top().first);
-        Node* node_added = acd_graph.getNode(comm.nodes_to_be_added.top().first);
+        // Get top elements from heapAndMap
+        Node* node_removed = acd_graph.getNode(comm.nodes_to_be_removed.getMaxElementId());
+        Node* node_added = acd_graph.getNode(comm.nodes_to_be_added.getMaxElementId());
 
         // Get the community of the added node
         int involved_comm_label = node_added->label;
         Community& involved_comm = communities.at(involved_comm_label);
 
-        double initial_modularity = modularityContributionByCommunity(comm) + modularityContributionByCommunity(involved_comm);
+        double initial_modularity = modularityContributionByCommunity(comm, totalEdges) + modularityContributionByCommunity(involved_comm, totalEdges);
 
         int r_1_in{0}, r_1_out{0}, r_2_in{0}, r_2_out{0};
         for (const auto& edge: node_removed->edgeList) {
+            // Ignore moving neighbor
+            if (edge.first->id == node_added->id) {
+                continue;
+            }
+
             if (edge.first->label == community_label) {
                 r_1_in += edge.second;
+                r_1_out -= edge.second;
             } else {
                 r_1_out += edge.second;
             }
 
             if (edge.first->label == involved_comm_label) {
                 r_2_in += edge.second;
+                r_2_out -= edge.second;
             } else {
                 r_2_out += edge.second;
             }
@@ -259,14 +247,21 @@ bool ApproximateCommunityDetection::nodeSwapAllowed(int community_label) {
 
         int a_1_in{0}, a_1_out{0}, a_2_in{0}, a_2_out{0};
         for (const auto& edge: node_added->edgeList) {
+            // Ignore moving neighbor
+            if (edge.first->id == node_removed->id) {
+                continue;
+            }
+
             if (edge.first->label == community_label) {
                 a_1_in += edge.second;
+                a_1_out -= edge.second;
             } else {
                 a_1_out += edge.second;
             }
 
             if (edge.first->label == involved_comm_label) {
                 a_2_in += edge.second;
+                a_2_out -= edge.second;
             } else {
                 a_2_out += edge.second;
             }
@@ -275,7 +270,7 @@ bool ApproximateCommunityDetection::nodeSwapAllowed(int community_label) {
         double final_modularity = getModularity(comm.e_in - r_1_in + a_1_in, comm.e_out - r_1_out + a_1_out, totalEdges) +
             getModularity(involved_comm.e_in - a_2_in + r_2_in, involved_comm.e_out - a_2_out + r_2_out, totalEdges);
 
-        if (final_modularity - initial_modularity > 0) {
+        if (final_modularity > initial_modularity) {
             return true;
         }
     } catch (const out_of_range& e) {
@@ -283,12 +278,4 @@ bool ApproximateCommunityDetection::nodeSwapAllowed(int community_label) {
     }
 
     return false;
-}
-
-double ApproximateCommunityDetection::modularityContributionByCommunity(const Community& comm) {
-    return getModularity(comm.e_in, comm.e_out, totalEdges);
-}
-
-double ApproximateCommunityDetection::getModularity(int e_in, int e_out, int total_edges) {
-    return (static_cast<double>(e_in) / total_edges) - pow((static_cast<double>(2.0 * e_in + e_out) / (2.0 * total_edges)), 2);
 }
